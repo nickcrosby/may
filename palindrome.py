02a2965854d835b3de43b2a8aee90fff393e083c
nickcrosby <36829818+nickcrosby@users.noreply.github.com>
2018-05-01 11:40:06 +1000

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,4 @@
-# This program checks a user input and if it is a
-# palindrome (reads the same forwards and backwards) then
-# it informs the user of such
+# This program checks a user input and if it is a palindrome (reads the same forwards and backwards).
 
 print("Palindrome - a word that is reads the same forwards and backwards")
 print("Enter a word and we'll check if its a palindrome: ")
@@ -15,9 +13,9 @@ def palindrome():
 palindrome()
 
 # Here we define a function under the name 'palindrome()' so that the user can try again if the input is not a palindrome.
-# Also, note that the syntax used on line 9 is an "extended slice", which allows the user input to be reversed.
-# This reversed input is stored under the string named 'rev'.
-# The program compares 'rev' to the original ('inp') to see if they are the same.
+# Also, note that the syntax used on line 7 is an "extended slice", which allows the user input to be easily reversed.
+# This reversed input is stored under the new string named 'rev'.
+# The program compares 'rev' to the original ('inp') in line 8 to see if they are the identical.
 # If they are identical, then the user input is a palindrome and the program informs the user of such.
 
 
